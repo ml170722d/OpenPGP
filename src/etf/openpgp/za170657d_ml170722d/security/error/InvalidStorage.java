de9b46781293f77a03f08319e7877672665de61f
Luka Matovic <ml170722d@student.etf.bg.ac.rs>
2021-05-29 20:44:54 +0200

    custom errors

---
+++ src/etf/openpgp/za170657d_ml170722d/security/error/InvalidStorage.java
@@ -0,0 +1,14 @@
+package etf.openpgp.za170657d_ml170722d.security.error;
+
+public class InvalidStorage extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidStorage() {
+		super("Storage saw not set");
+	}
+	
+}
