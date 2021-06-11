bdec260c79452cc197d775bfdf65aba3c988f72d
Zivkovic013 <azivkovic013@gmail.com>
2021-06-12 01:31:45 +0200

    Decryption done connected with GUI
    Job done

--- src/etf/openpgp/za170657d_ml170722d/GUI/ReceiveMessageDialog.java
+++ src/etf/openpgp/za170657d_ml170722d/GUI/ReceiveMessageDialog.java
@@ -10,6 +10,8 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import etf.openpgp.za170657d_ml170722d.securityV2.Decryptor;
+import etf.openpgp.za170657d_ml170722d.securityV2.Decryptor.Info;
+import etf.openpgp.za170657d_ml170722d.securityV2.KeyChain;
 
 import java.awt.Toolkit;
 import javax.swing.JLabel;
@@ -23,20 +25,13 @@ import javax.swing.border.LineBorder;
 public class ReceiveMessageDialog extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	
 	private File selectedFile;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		try {
-			ReceiveMessageDialog dialog = new ReceiveMessageDialog();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+
 
 	/**
 	 * Create the dialog.
@@ -51,9 +46,83 @@ public class ReceiveMessageDialog extends JDialog {
 		getContentPane().add(contentPanel);
 		contentPanel.setLayout(null);
 		
+		JLabel lblEncrypt = new JLabel("Encryption status : unknown");
+		lblEncrypt.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblEncrypt.setBounds(18, 200, 275, 33);
+		contentPanel.add(lblEncrypt);
+		
+		JLabel lblSignature = new JLabel("Signature status : unknown");
+		lblSignature.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblSignature.setBounds(18, 250, 275, 33);
+		contentPanel.add(lblSignature);
+		
 		JLabel selectedFileLabel = new JLabel("Selected File :");
 		selectedFileLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
 		selectedFileLabel.setBounds(334, 38, 242, 33);
+		
+		JButton btnSaveResult = new JButton("SAVE");
+		btnSaveResult.setEnabled(false);
+		btnSaveResult.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		JLabel lblUserStatus = new JLabel("User status : unknown");
+		lblUserStatus.setBounds(18, 300, 263, 33);
+		contentPanel.add(lblUserStatus);
+		lblUserStatus.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		
+		JButton btnDecrpyt = new JButton("DECRYPT");
+		btnDecrpyt.setEnabled(false);
+		btnDecrpyt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+	
+				System.out.println("Apsolutni : " + selectedFile.getAbsolutePath() + " Neki drugi : " + new File("").getAbsolutePath());
+				
+				Info info_item = null;
+				
+				try {
+					info_item = Decryptor.dectyprFile(selectedFile.getAbsolutePath(), new File("").getAbsolutePath());
+					
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();		
+				}
+				
+				if(info_item == null) {
+					lblEncrypt.setText("Encryption status : error with the file");
+					lblSignature.setText("Signature status : error with the file");
+					lblUserStatus.setText("User status :  error with the file");
+					return;
+					
+				}
+				
+				if(info_item.isEncrypted()) {
+					lblEncrypt.setText("Encryption status : message encrypted");
+				}
+				else {
+					lblEncrypt.setText("Encryption status : message not encrypted");
+				}
+				
+				if(info_item.isSigned()) {
+					lblSignature.setText("Signature status : message signed");
+				}
+				else {
+					lblSignature.setText("Signature status : message not signed");
+				}
+				
+				//lblUserStatus.setText("User status : " + KeyChain.getKeyRing(info_item.getId()).getUserId());
+				String arrSplit[] = KeyChain.getKeyRing(info_item.getId()).getUserId().split("<");
+				lblUserStatus.setText("User status : " + arrSplit[0].toString() + " - " + arrSplit[1].substring(0, arrSplit[1].length() - 1));
+				btnSaveResult.setEnabled(true);;
+			}
+		});
+		btnDecrpyt.setBackground(Color.MAGENTA);
+		btnDecrpyt.setForeground(Color.CYAN);
+		btnDecrpyt.setFont(new Font("Tahoma", Font.BOLD, 16));
+		btnDecrpyt.setBounds(71, 114, 135, 50);
+		contentPanel.add(btnDecrpyt);
 		
 		
 		JButton btnNewButton = new JButton("SELECT FILE");
@@ -64,18 +133,13 @@ public class ReceiveMessageDialog extends JDialog {
 				if(result == JFileChooser.APPROVE_OPTION) {
 					selectedFile = fileChooser.getSelectedFile();
 					selectedFileLabel.setText("Selected File : " + selectedFile.getName());
-					
+					btnDecrpyt.setEnabled(true);
 				}
 			}
 		});
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 15));
 		btnNewButton.setBounds(180, 38, 142, 33);
 		contentPanel.add(btnNewButton);
-		
-		JLabel lblUserStatus = new JLabel("User status : unknown");
-		lblUserStatus.setBounds(18, 300, 230, 33);
-		contentPanel.add(lblUserStatus);
-		lblUserStatus.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		
 		JLabel lblNewLabel = new JLabel("Choose .pgp file");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
@@ -84,41 +148,6 @@ public class ReceiveMessageDialog extends JDialog {
 		
 		
 		contentPanel.add(selectedFileLabel);
-		
-		JButton btnDecrpyt = new JButton("DECRYPT");
-		btnDecrpyt.setEnabled(false);
-		btnDecrpyt.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-	
-				//Decryptor.dectyprFile(selectedFile.getAbsolutePath(),outputFilePath);
-				System.out.println("Apsolutni : " + selectedFile.getAbsolutePath() + " Neki drugi : " + new File("").getAbsolutePath());
-				//Decryptor.dectyprFile(inputFilePath, outputFilePath);
-	
-				
-			}
-		});
-		btnDecrpyt.setBackground(Color.MAGENTA);
-		btnDecrpyt.setForeground(Color.CYAN);
-		btnDecrpyt.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btnDecrpyt.setBounds(71, 114, 135, 50);
-		contentPanel.add(btnDecrpyt);
-		
-		JLabel lblEncrypt = new JLabel("Encryption status : unknown");
-		lblEncrypt.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblEncrypt.setBounds(18, 200, 230, 33);
-		contentPanel.add(lblEncrypt);
-		
-		JLabel lblSignature = new JLabel("Signature status : unknown");
-		lblSignature.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblSignature.setBounds(18, 250, 230, 33);
-		contentPanel.add(lblSignature);
-		
-		JButton btnSaveResult = new JButton("SAVE");
-		btnSaveResult.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnSaveResult.setEnabled(false);
 		btnSaveResult.setBackground(Color.RED);
 		btnSaveResult.setForeground(Color.CYAN);
 		btnSaveResult.setFont(new Font("Tahoma", Font.BOLD, 16));
