fa9f3f29d80ce35d5f8e45c13ac5df97ec6327d7
Zivkovic013 <azivkovic013@gmail.com>
2021-06-11 22:42:49 +0200

    GUI for Receive Message done
    --connection with backend needed.

--- src/etf/openpgp/za170657d_ml170722d/GUI/ReceiveMessageDialog.java
+++ src/etf/openpgp/za170657d_ml170722d/GUI/ReceiveMessageDialog.java
@@ -18,6 +18,7 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+import javax.swing.border.LineBorder;
 
 public class ReceiveMessageDialog extends JDialog {
 
@@ -43,15 +44,15 @@ public class ReceiveMessageDialog extends JDialog {
 	public ReceiveMessageDialog() {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(ReceiveMessageDialog.class.getResource("/com/sun/java/swing/plaf/windows/icons/Computer.gif")));
 		setTitle("Receive Message");
-		setBounds(100, 100, 594, 331);
+		setBounds(100, 100, 598, 451);
 		getContentPane().setLayout(null);
-		contentPanel.setBounds(0, 0, 576, 249);
+		contentPanel.setBounds(0, 0, 576, 366);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel);
 		contentPanel.setLayout(null);
 		
 		JLabel selectedFileLabel = new JLabel("Selected File :");
-		selectedFileLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		selectedFileLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
 		selectedFileLabel.setBounds(334, 38, 242, 33);
 		
 		
@@ -71,7 +72,12 @@ public class ReceiveMessageDialog extends JDialog {
 		btnNewButton.setBounds(180, 38, 142, 33);
 		contentPanel.add(btnNewButton);
 		
-		JLabel lblNewLabel = new JLabel("Choose .pgp file  :");
+		JLabel lblUserStatus = new JLabel("User status : unknown");
+		lblUserStatus.setBounds(18, 300, 230, 33);
+		contentPanel.add(lblUserStatus);
+		lblUserStatus.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		
+		JLabel lblNewLabel = new JLabel("Choose .pgp file");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblNewLabel.setBounds(18, 38, 150, 33);
 		contentPanel.add(lblNewLabel);
@@ -79,35 +85,65 @@ public class ReceiveMessageDialog extends JDialog {
 		
 		contentPanel.add(selectedFileLabel);
 		
-		JButton btnNewButton_1 = new JButton("DECRYPT");
-		btnNewButton_1.addActionListener(new ActionListener() {
+		JButton btnDecrpyt = new JButton("DECRYPT");
+		btnDecrpyt.setEnabled(false);
+		btnDecrpyt.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 	
 				//Decryptor.dectyprFile(selectedFile.getAbsolutePath(),outputFilePath);
 				System.out.println("Apsolutni : " + selectedFile.getAbsolutePath() + " Neki drugi : " + new File("").getAbsolutePath());
 				//Decryptor.dectyprFile(inputFilePath, outputFilePath);
-				
+	
 				
 			}
 		});
-		btnNewButton_1.setBackground(Color.MAGENTA);
-		btnNewButton_1.setForeground(Color.CYAN);
-		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btnNewButton_1.setBounds(210, 106, 135, 50);
-		contentPanel.add(btnNewButton_1);
+		btnDecrpyt.setBackground(Color.MAGENTA);
+		btnDecrpyt.setForeground(Color.CYAN);
+		btnDecrpyt.setFont(new Font("Tahoma", Font.BOLD, 16));
+		btnDecrpyt.setBounds(71, 114, 135, 50);
+		contentPanel.add(btnDecrpyt);
+		
+		JLabel lblEncrypt = new JLabel("Encryption status : unknown");
+		lblEncrypt.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblEncrypt.setBounds(18, 200, 230, 33);
+		contentPanel.add(lblEncrypt);
+		
+		JLabel lblSignature = new JLabel("Signature status : unknown");
+		lblSignature.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblSignature.setBounds(18, 250, 230, 33);
+		contentPanel.add(lblSignature);
+		
+		JButton btnSaveResult = new JButton("SAVE");
+		btnSaveResult.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnSaveResult.setEnabled(false);
+		btnSaveResult.setBackground(Color.RED);
+		btnSaveResult.setForeground(Color.CYAN);
+		btnSaveResult.setFont(new Font("Tahoma", Font.BOLD, 16));
+		btnSaveResult.setBounds(355, 114, 135, 50);
+		contentPanel.add(btnSaveResult);
 		{
 			JPanel buttonPane = new JPanel();
-			buttonPane.setBounds(0, 249, 576, 35);
+			buttonPane.setBounds(0, 369, 576, 35);
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane);
 			{
 				JButton okButton = new JButton("OK");
+				okButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
 				okButton.setActionCommand("OK");
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				cancelButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
